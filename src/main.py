11580711b8e03eb6f7c25973a4fb9f82dbb03474
Farzam <farzamgt@gmail.com>
2025-10-13 18:22:56 +0300

    refactor: fix directory naming from routes to routers

--- src/main.py
+++ src/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
+from routers import accounts
 
 
 app = FastAPI(title="Cinema Api", description="Description of project")
 
 api_version_prefix = "/api/v1"
+
+app.include_router(accounts.router, prefix=f"{api_version_prefix}/accounts", tags=["Accounts"])
