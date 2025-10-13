46f8f9d0a0ef26fd530c46b47edc6a5fb734cc4c
MakovM <makovinskiy09@gmail.com>
2025-10-13 11:10:12 +0300

    Add black to CI Pipeline

--- src/main.py
+++ src/main.py
@@ -1,9 +1,6 @@
 from fastapi import FastAPI
 
 
-app = FastAPI(
-    title="Cinema Api",
-    description="Description of project"
-)
+app = FastAPI(title="Cinema Api", description="Description of project")
 
 api_version_prefix = "/api/v1"
