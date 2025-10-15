dda50c58bd9c96b31552bd2788f11d13c11affd3
MakovM <makovinskiy09@gmail.com>
2025-10-15 14:49:49 +0300

    Basic Celery and other services setup

---
+++ src/celery_worker.py
@@ -0,0 +1,16 @@
+from typing import Any
+
+from celery import Celery
+from celery.schedules import crontab
+
+from celery_conf import celery_app
+from src.tasks import task_for_clean_tokens
+
+
+@celery_app.on_after_configure.connect
+def setup_periodic_task(sender: Celery, **kwargs: Any) -> None:
+    sender.add_periodic_task(
+        crontab(hour=9, minute=0),
+        task_for_clean_tokens.s(),
+        name="Delete expired token"
+    )
