1937d7535104691cf61b016defbbf84ada17864c
MakovM <makovinskiy09@gmail.com>
2025-10-19 20:23:31 +0300

    CD Pipeline testing

--- src/routers/carts.py
+++ src/routers/carts.py
@@ -10,7 +10,7 @@ from database import get_db, UserGroupEnum, OrderItemModel, OrderModel, OrderSta
 from security.dependencies import get_current_user, ModerAdminUser
 
 
-router = APIRouter(prefix="/cart")
+router = APIRouter(prefix="/carts")
 
 
 @router.get("/", response_model=CartResponse)
